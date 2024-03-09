a7aff5b76640c3dd977f9c4f4ef1a7c5af495d41
nic0-dev <mlcagas@up.edu.ph>
2024-03-10 00:48:11 +0800

    Account Dropdown

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -6,8 +6,12 @@
 	import { page } from "$app/stores"
 
 	let visible = true;
+	let visible_account = true;
 	function toggleMenu() {
 		visible = !visible
+	}
+	function toggleAccount() {
+		visible_account = !visible_account
 	}
 </script>
 
@@ -63,25 +67,27 @@
 	<div class="flex items-center gap-4">
 		<div class="text-sm flex gap-4 justify-between py-1">
 
-			<div>
-				<div class="relative w-10 h-10 overflow-hidden bg-gray-100 rounded-full cursor-pointer">
-					<svg class="absolute w-12 h-12 text-gray-400 -left-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd"></path></svg>
-				</div>
-	
-				<div class="absolute right-0 z-10 mt-4 mx-4 sm:mx-8 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">
-					<div class="text=gray=700 block  px-4 py-2 text-sm" role="none">
-						Signed in as<br>user@example.com
+			<div >
+				<button on:click={toggleAccount} data-collapse-toggle="account-menu" type="button"  class="relative w-10 h-10 overflow-hidden bg-gray-100 rounded-full cursor-pointer">
+					<svg class="relative w-12 h-12 text-gray-400 -left-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd"></path></svg>
+				</button>
+				
+				{#if !visible_account}
+					<div class="absolute right-0 z-10 mt-4 mx-4 sm:mx-8 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">
+						<div class="text=gray=700 block  px-4 py-2 text-sm" role="none">
+							Signed in as<br>user@example.com
+						</div>
+						<div class="py-1" role="none">
+						<a href="/" class="text-gray-500 block px-4 py-2 text-sm hover:bg-gray-100 hover:text-gray-900" role="menuitem" tabindex="-1" id="menu-item-2">Account</a>
+						<a href="/" class="text-gray-500 block px-4 py-2 text-sm hover:bg-gray-100 hover:text-gray-900" role="menuitem" tabindex="-1" id="menu-item-3">Support</a>
+						<a href="/" class="text-gray-500 block px-4 py-2 text-sm hover:bg-gray-100 hover:text-gray-900" role="menuitem" tabindex="-1" id="menu-item-3">Settings</a>
+						</div>
+						<div class="py-1" role="none">
+						<a href="/" class="text-gray-500 block px-4 py-2 text-sm hover:bg-gray-100 hover:text-gray-900" role="menuitem" tabindex="-1" id="menu-item-6">Sign out</a>
+						</div>
 					</div>
-					<div class="py-1" role="none">
-					  <a href="/" class="text-gray-500 block px-4 py-2 text-sm hover:bg-gray-100 hover:text-gray-900" role="menuitem" tabindex="-1" id="menu-item-2">Account</a>
-					  <a href="/" class="text-gray-500 block px-4 py-2 text-sm hover:bg-gray-100 hover:text-gray-900" role="menuitem" tabindex="-1" id="menu-item-3">Support</a>
-					  <a href="/" class="text-gray-500 block px-4 py-2 text-sm hover:bg-gray-100 hover:text-gray-900" role="menuitem" tabindex="-1" id="menu-item-3">Settings</a>
-					</div>
-					<div class="py-1" role="none">
-					  <a href="/" class="text-gray-500 block px-4 py-2 text-sm hover:bg-gray-100 hover:text-gray-900" role="menuitem" tabindex="-1" id="menu-item-6">Sign out</a>
-					</div>
-				  </div>
-				</div>
+				{/if}
+			</div>
 		</div>
 	</div>
 </header>
